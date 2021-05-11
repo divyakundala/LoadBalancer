27c7c68e8c5c56e46b10cab4f9f15bb3acc61108
divyakundala <kundaladivya@gmail.com>
2021-05-11 13:50:00 -0400

    Create containers

--- ans1.py
+++ ans1.py
@@ -12,27 +12,47 @@ def delete_container(container_name, password):
     command = "ansible-playbook -i hosts del.yml -e container=" + container_name + " -e password=" + password
     subprocess.call([command],shell=True)
 
+def create_vethpair(c, password):
+    command="ansible-playbook -i hosts veth.yml -e veth1=" + c[1] + " -e veth2="+ c[2] + " -e container1=" + c[0][0] + " -e container2=" + c[0][1] + " -e password=" + password  
+    subprocess.call([command],shell=True) 
+    
+def delete_vethpair(veth1, password):
+    command="ansible-playbook -i hosts vethdel.yml -e veth1=" + veth1 + " -e password=" + password  
+    subprocess.call([command],shell=True)    
+    
 if __name__ == '__main__':
     n = len(sys.argv)
     a=sys.argv[1]
     password = getpass.getpass(prompt='Password: ', stream=None)
     header=[]
-    rows = []
+    conn= []
+    row_num = 0
     with open("toplology.csv") as read_obj:
         csv_reader = reader(read_obj)
         for row in csv_reader:
            if header == []:
                header = row
            else:
-               rows.append(row)
+               for col in range(row_num,len(row)):
+                   if row[col] == '1':
+                       conn.append([(header[row_num], header[col]), header[row_num] + header[col], header[col] + header[row_num]])
+               row_num +=1    
 
+    print(conn)
     if a == "create":
         # create containers
         for container in header:
             create_container(container, password)
-
+        
+        # create veth pairs
+        for c in conn:
+            create_vethpair(c, password)
+            
     elif a == "delete":
         # delete containers
         for container in header:
             delete_container(container, password)
-
+            
+        # delete veth pairs
+        for c in conn:
+            delete_vethpair(c[1], password)
---
+++ dockerfile
@@ -0,0 +1,34 @@
+FROM ubuntu
+
+MAINTAINER divya kundala
+
+RUN apt-get update
+RUN apt-get -y --force-yes install iproute2
+RUN apt-get -y --force-yes install telnet
+RUN apt-get -y --force-yes install openssh-server
+RUN apt-get -y --force-yes install iptables
+RUN apt-get -y --force-yes install iputils-ping
+RUN apt-get -y --force-yes install traceroute
+RUN apt-get -y --force-yes install tcpdump
+RUN apt-get -y --force-yes install iperf
+RUN apt-get -y --force-yes install vim
+RUN apt-get -y --force-yes install python3
+RUN apt-get -y --force-yes install python3-pip
+RUN apt-get -y --force-yes install python3-pexpect
+RUN pip3 install paramiko
+RUN apt-get -y --force-yes install nano
+
+RUN apt-get -y --force-yes install libc-ares2
+RUN apt-get -y --force-yes install libpci3
+RUN apt-get -y --force-yes install libsnmp-base
+
+
+
+
+RUN mv /usr/sbin/tcpdump /usr/bin/tcpdump
+RUN ln -s /usr/bin/tcpdump /usr/sbin/tcpdump
+
+RUN sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+
+
